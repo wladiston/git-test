3cb93400769b5d5f1bf5e9c2384d0419f29e5bf9
Wlad Paiva <desenvolvedormg@gmail.com>
2022-11-04 19:04:26 -0300

    my feature 2

--- test.js
+++ test.js
@@ -2,6 +2,11 @@ export default function () {
   return <div>abc</div>;
 }
 
+// add a random function
+export function random() {
+  return Math.random();
+}
+
 export const getStaticProps = async () => {
   return {
     props: {
