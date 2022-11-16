b48e70b4aca07b97d8357f399b810cec0c3b0c6a
💙 Typescript <dev@swan.com>
2022-11-16 17:23:42 -0300

    Renaming files to ".ts"

--- src/test.js
+++
@@ -1,5 +0,0 @@
-// calculate random bitcoin wallet address
-var bitcoin = require("bitcoinjs-lib");
-var keyPair = bitcoin.ECPair.makeRandom();
-var address = keyPair.getAddress();
-console.log(address);
