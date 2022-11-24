741d671ea0b22d1644df01997119f51e994918e5
Wlad Paiva <desenvolvedormg@gmail.com>
2022-11-24 16:33:21 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const realEnv = (x: $TSFixMe) => {
   switch (x) {
-    case "app.swanbitcoin.com":
-    case "app.prod.swanbitcoin.com":
+    case "app.com":
+    case "app.prod.com":
       return "production";
-    case "app.qa.swanbitcoin.com":
+    case "app.qa.com":
       return "qa";
     default:
       return "dev";
